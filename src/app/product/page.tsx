6bcda9834ac3fe4884687cf50b3ee4791b6ecf7d
Dominik Rubröder <dominik.rubroeder@mediawave.de>
2023-11-17 16:13:55 +0100

    Fix missing ProductCard prop 'priority'

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,15 +4,15 @@ import { useGetAllProducts } from "@/hooks/useGetAllProducts";
 import ProductCard from "@/components/ProductCard";
 
 export default function ProductPage() {
-  const prodcuts = useGetAllProducts();
+  const products = useGetAllProducts();
   return (
     <div className="p-8">
       <h1 className="text-2xl">All products</h1>
       <section>
         <ul className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {prodcuts.map((product) => (
+          {products.map((product) => (
             <li key={product.id}>
-              <ProductCard product={product} />
+              <ProductCard product={product} priority={true} />
             </li>
           ))}
         </ul>
